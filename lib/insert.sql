4cfa3333305d598cf8d4fbf031a2d2b9f1091cf7
Jennifer Gifford <jgifford82@gmail.com>
2022-10-25 07:50:31 -0600

    commit tree faa3f5d4f6f27322e5d5427490e05f15072feaeb

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'F', 'black', 'fun', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'brown', 'tough', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'black', 'fierce', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'green', 'mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'white', 'murderous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'F', 'black', 'rough', 1);
